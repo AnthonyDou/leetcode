6ddff2517d8d3ba23ba800d7cc6068e638611a64
destin <me@destin.im>
2015-02-27 01:21:33 +0800

    add new solutions

--- 180-consecutive-numbers.sql
+++ 180-consecutive-numbers.sql
@@ -19,14 +19,18 @@ Write a SQL query to find all numbers that appear at least three times consecuti
 
 For example, given the above Logs table, 1 is the only number that appears consecutively for at least three times.
 
+**
+
+本题可以先利用 178. Rank Scores 的方法求出临时表，再对该表进行分组判断。
+
 */
 
 # Write your MySQL query statement below
 select distinct Num from (
-    select Num, count(ct) as Times from (
-        select num, @cur := @cur + if(@prev = Num, 0, 1) as ct, @prev := Num
-        from Logs s, (select @cur := 0) c, (SELECT @prev := null) p
-        order by Id asc
-    ) t
-    group by ct having Times >= 3
-) n;
+	select Num, count(Rank) as Times from (
+		select num, @cur := @cur + if(@prev = Num, 0, 1) as Rank, @prev := Num
+		from Logs s, (select @cur := 0) c, (select @prev := null) p
+		order by Id asc
+	) t
+	group by Rank having Times >= 3
+) tt;
